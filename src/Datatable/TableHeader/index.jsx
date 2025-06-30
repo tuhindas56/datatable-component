79c38f1683f74bea2f8c73b3492db72eee2f6c17
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-06-30 13:01:28 +0530

    feat: update Filters component with searchable menu

--- src/Datatable/TableHeader/index.jsx
+++ src/Datatable/TableHeader/index.jsx
@@ -1,11 +1,13 @@
 import Stack from "@mui/material/Stack"
 
+import Filters from "./Filter"
 import Search from "./Search"
 import ViewMenu from "./ViewMenu"
 
 const TableHeader = ({ table, setSearchQuery }) => {
   return (
     <Stack spacing={2}>
+      <Filters />
       <Stack direction="row" justifyContent="space-between">
         <Search placeholder="Search products..." setSearchQuery={setSearchQuery} />
         <ViewMenu table={table} />
