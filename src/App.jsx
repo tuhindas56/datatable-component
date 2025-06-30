be2842712a77a1c4e0c9d04f3bf95f9a4b4d6b74
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-06-30 15:49:37 +0530

    feat: implement basic table

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,56 @@
+import { useMemo } from "react"
+
+import useData from "./hooks/useData"
+
+import Datatable from "./Datatable"
+
 const App = () => {
-  return <div>App</div>
+  const columns = useMemo(
+    () => [
+      {
+        accessorFn: row => row.product,
+        header: "Product",
+      },
+      {
+        accessorFn: row => row.price,
+        header: "Price",
+      },
+      {
+        accessorFn: row => row.material,
+        header: "Material",
+      },
+      {
+        accessorFn: row => row.department,
+        header: "Department",
+      },
+      {
+        accessorFn: row => row.title,
+        header: "Title",
+      },
+      {
+        accessorFn: row => row.author,
+        header: "Author",
+      },
+      {
+        accessorFn: row => row.publisher,
+        header: "Publisher",
+      },
+      {
+        accessorFn: row => row.genre,
+        header: "Genre",
+      },
+    ],
+    []
+  )
+
+  const { loading, error, data } = useData({
+    rowsPerPage: 100,
+  })
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <Datatable columns={columns} loading={loading} data={data?.data} error={error} />
+    </div>
+  )
 }
 export default App
---
+++ src/Datatable/index.jsx
@@ -0,0 +1,60 @@
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+
+import styles from "./styles.module.css"
+
+const Datatable = ({ loading = false, error = null, data = [], columns = [] }) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getRowId: row => row.id,
+  })
+  const colCount = table.getHeaderGroups()[0].headers.length
+
+  return (
+    <div className={styles["ts-dt-table-container"]}>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => {
+            return (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return <th key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</th>
+                })}
+                {}
+              </tr>
+            )
+          })}
+        </thead>
+
+        <tbody>
+          {table.getCoreRowModel().rows.map(row => {
+            if (loading)
+              return (
+                <tr>
+                  <td colSpan={colCount}>Loading..</td>
+                </tr>
+              )
+
+            if (error)
+              return (
+                <tr>
+                  <td colSpan={colCount}>There was an error.</td>
+                </tr>
+              )
+
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => {
+                  return <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Datatable
