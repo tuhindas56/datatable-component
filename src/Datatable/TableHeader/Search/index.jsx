5378490b1526a2449356cba238a76dec97832563
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-07-02 19:34:50 +0530

    feat: updated search input styling

--- src/Datatable/TableHeader/Search/index.jsx
+++ src/Datatable/TableHeader/Search/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
-import Input from "@mui/material/Input"
+import { Input } from "reactstrap"
+
+import styles from "./styles.module.css"
 
 const Search = ({ placeholder, setSearchQuery }) => {
   const [search, setSearch] = useState("")
@@ -14,6 +16,14 @@ const Search = ({ placeholder, setSearchQuery }) => {
     return () => clearTimeout(timeout)
   }, [search])
 
-  return <Input type="text" value={search} onChange={handleInputChange} placeholder={placeholder} />
+  return (
+    <Input
+      type="text"
+      value={search}
+      onChange={handleInputChange}
+      placeholder={placeholder}
+      className={styles["table-search-input"]}
+    />
+  )
 }
 export default Search
